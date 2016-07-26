e4f2f9a165fadf4240d069943ce30ac5ee779cf1
Ian Gregson <iangregson@formedix.com>
2016-07-26 16:03:13 +0100

    added mergeSort

---
+++ linkedList.js
@@ -0,0 +1,17 @@
+// isEmpty
+// size
+// prePend
+// append
+// remove
+// contains
+// print
+
+function LinkedList() {
+    
+}
+
+LinkedList.prototype.isEmpty = () => this.head == null 
+
+let list = new LinkedList()
+
+console.log(list.isEmpty())
