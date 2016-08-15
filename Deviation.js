5f96b607305f4b3323f1d45b501774030fffce0d
Ian Gregson <iangregson@formedix.com>
2016-08-15 13:13:32 +0100

    Deviation

---
+++ Deviation.js
@@ -0,0 +1,12 @@
+// 1. Split the array into an array f sequences of length d
+// 2. Find the min and max in each array, push the median into a new array
+// 3. Find the maximum value in the new median array
+
+function splitArray(arr, d) {
+
+}
+
+
+
+
+const v = [6,9,4,7,4,1]
