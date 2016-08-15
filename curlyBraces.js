755a5c6bedf7a5bb45137a6679469b98658feb0b
Ian Gregson <ian@gregson.me>
2016-08-15 10:03:09 +0100

    Kadane + curly braces

--- curlyBraces.js
+++ curlyBraces.js
@@ -7,11 +7,16 @@ function checkBalanced(s) {
     for (i = 0; c = s.charAt(i); i++) {
         bracePosition = parens.indexOf(c)
 
+        // Get out of this iteration of the loop if this characer isn't a brace
         if (bracePosition === -1) continue
-        
+
+        // If the brace has an even numbered position, let's track the position + 1        
         if (bracePosition % 2 === 0) stack.push(bracePosition + 1)
+        // All the odd numbered bracePositions should close a brace that's been added to the tracker 
+        // If it doesn't, we can escape the function returning false to indicate the string doesn't have balanced parens
         else if (stack.pop() !== bracePosition) return false
     }
+    // if there's nothing in our tracker when we're done, the string can be called balanced
     return stack.length === 0
 }
 
